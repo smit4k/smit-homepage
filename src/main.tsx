059d4c670e8481f9cbf3c24c2e37e83862c186c7
smit4k <smit.patil.usa@gmail.com>
2025-07-08 23:10:01 -0400

    Add syntax highlighting for code blocks in Markdown (MDX) with VSCode Dark+ Theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import 'prism-themes/themes/prism-vsc-dark-plus.css'; // VSCode Dark+ theme
 
 createRoot(document.getElementById("root")!).render(<App />);
--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import mdx from "@mdx-js/rollup"
 import remarkGfm from 'remark-gfm'
+import rehypePrism from 'rehype-prism-plus';
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
@@ -14,6 +15,7 @@ export default defineConfig(({ mode }) => ({
   plugins: [
     mdx({
       remarkPlugins: [remarkGfm],
+      rehypePlugins: [rehypePrism,],
     }),
     react(),
     mode === 'development' &&
